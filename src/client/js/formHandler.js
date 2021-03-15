20021113d4c592b6c55bb0364b99819f1ce0395a
arlw87 <arlw87@googlemail.com>
2021-03-15 18:28:49 +0000

    feat: Added a formHandler js file for handling user submissions
    The formHandler uses the fetch API to post the form data to the
    node / express server to process the form data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,6 +8,42 @@ searchBtn.addEventListener('click', (event) => {
     //handle form validation here
     //TODO: user input validation
 
+
     //get the user input data
+    const destination = document.querySelector('#destination').value;
+    const date = document.querySelector('#date').value;
+
+    const sendObj = {
+        destination: destination,
+        date: date
+    }
+
+    postData('http://localhost:8000/query', sendObj)
+        .catch((error) => {
+            console.log(error);
+        });
 
 });
+
+/**
+ * Send a post request in JSON format to the defined url 
+ * @param {string} url 
+ * @param {object} data 
+ */
+ const postData = async(url, data) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    try{
+        const newData = await response.json();
+        return newData;
+    }catch(error){
+        throw Error(error);
+    }
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static('dist'));
 
