50adf33059ab870d35410080860e4c06b70dc10a
arlw87 <arlw87@googlemail.com>
2021-04-02 10:41:37 +0100

    refactor: Added comments to hero.js file and added code block to make variables private.

--- src/client/js/hero.js
+++ src/client/js/hero.js
@@ -1,10 +1,19 @@
-//interactive webpage
-const formSection = document.querySelector('#form-section');
-const callToAction = document.querySelector('.call-to-action');
+//function used in hero banner
 
-callToAction.addEventListener('click', (event)=> {
-    formSection.scrollIntoView({
-        behavior: "smooth"
+//use code blocks to make variables private
+{
+    const formSection = document.querySelector('#form-section');
+    const callToAction = document.querySelector('.call-to-action');
+
+    /**
+     * Add click listner to the 'Call to Action' button, to scroll the page down
+     * to the form section when clicked. 
+     */
+    callToAction.addEventListener('click', (event)=> {
+        formSection.scrollIntoView({
+            behavior: "smooth"
+        });
     });
-});
+}
+
 
