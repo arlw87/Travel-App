5ef8fcf028367015b606efff2c9acb80883cd65a
arlw87 <arlw87@googlemail.com>
2021-04-02 10:33:01 +0100

    refactor: Added comments to errorHandling file

--- src/client/js/errorHandling.js
+++ src/client/js/errorHandling.js
@@ -1,4 +1,12 @@
-//error handling
+//file contains functions to deal with error handling
+
+/**
+ * Displays an error message box over the form section of the web app when an error occurs. By adding
+ * the class error-display, to the error handling div, which is currently not displayed the element 
+ * will display.
+ * @param {string} errorMessage - Message to display in the error message box 
+ * @returns 
+ */
 export const displayError = (errorMessage) => {
     //display the error element
     const errorDisplay = document.querySelector('.error-handler-background');
@@ -11,12 +19,15 @@ export const displayError = (errorMessage) => {
 }
 
 
-//error message button event
+/**
+ * Creates an on click listener for the error message box close button. When the button, an 'X' in
+ * the top right hand corner of the message box is clicked the message box is closed. This is
+ * achieved by setting and removing classes on the error-handler div. 
+ */
 document.querySelector('.error-button').addEventListener('click', (event) => {
-
     //remove the error display
     const errorDisplay = document.querySelector('.error-handler-background');
     errorDisplay.classList.remove('error-display');
     errorDisplay.classList.add('error-non-display');
-
+    return;
 });
